3f5d22398bb5f68642c994c8a4d96e81f77418a7
tavomovi <144753780+tavomovi@users.noreply.github.com>
2025-10-27 17:54:55 -0600

    Update

--- Controllers/SitesController.cs
+++ Controllers/SitesController.cs
@@ -48,7 +48,7 @@ namespace InventorySys.Controllers
             return View();
         }
 
-        // POST: Sites/Create
+        // 
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
