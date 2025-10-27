b0022c739a7d16ebc2072b1404fd171b2ea44435
tavomovi <144753780+tavomovi@users.noreply.github.com>
2025-10-27 17:52:37 -0600

    Fuatere: Se crea controlador y vista de Sites por default

---
+++ Controllers/SitesController.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using InventorySys.Models;
+
+namespace InventorySys.Controllers
+{
+    public class SitesController : Controller
+    {
+        private readonly InventorySysContext _context;
+
+        public SitesController(InventorySysContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Sites
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.TblSites.ToListAsync());
+        }
+
+        // GET: Sites/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tblSite = await _context.TblSites
+                .FirstOrDefaultAsync(m => m.SiteId == id);
+            if (tblSite == null)
+            {
+                return NotFound();
+            }
+
+            return View(tblSite);
+        }
+
+        // GET: Sites/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Sites/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("SiteId,SiteName,SiteLocation,SiteActive")] TblSite tblSite)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(tblSite);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(tblSite);
+        }
+
+        // GET: Sites/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tblSite = await _context.TblSites.FindAsync(id);
+            if (tblSite == null)
+            {
+                return NotFound();
+            }
+            return View(tblSite);
+        }
+
+        // POST: Sites/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("SiteId,SiteName,SiteLocation,SiteActive")] TblSite tblSite)
+        {
+            if (id != tblSite.SiteId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(tblSite);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TblSiteExists(tblSite.SiteId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(tblSite);
+        }
+
+        // GET: Sites/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tblSite = await _context.TblSites
+                .FirstOrDefaultAsync(m => m.SiteId == id);
+            if (tblSite == null)
+            {
+                return NotFound();
+            }
+
+            return View(tblSite);
+        }
+
+        // POST: Sites/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var tblSite = await _context.TblSites.FindAsync(id);
+            if (tblSite != null)
+            {
+                _context.TblSites.Remove(tblSite);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TblSiteExists(int id)
+        {
+            return _context.TblSites.Any(e => e.SiteId == id);
+        }
+    }
+}
