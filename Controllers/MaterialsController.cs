353b38df75f95af936ded809ccc0607edd52fb43
luis-chm <luischavesmora@gmail.com>
2025-10-29 18:15:58 -0600

    feat: se modifica logica controller Materials

--- Controllers/MaterialsController.cs
+++ Controllers/MaterialsController.cs
@@ -1,182 +1,323 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using InventorySys.Helpers;
+using InventorySys.Models;
+using InventorySys.ViewModels;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
-using InventorySys.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
 
 namespace InventorySys.Controllers
 {
+    [Authorize]
     public class MaterialsController : Controller
     {
         private readonly InventorySysContext _context;
+        private readonly IWebHostEnvironment _environment;
 
-        public MaterialsController(InventorySysContext context)
+        public MaterialsController(InventorySysContext context, IWebHostEnvironment environment)
         {
             _context = context;
+            _environment = environment;
         }
 
-        // GET: Materials
+        private string GetUserRole()
+        {
+            return User.FindFirst(ClaimTypes.Role)?.Value ?? "";
+        }
+
         public async Task<IActionResult> Index()
         {
-            var inventorySysContext = _context.TblMaterials.Include(t => t.Collection).Include(t => t.Finiture).Include(t => t.Format).Include(t => t.Site).Include(t => t.User);
-            return View(await inventorySysContext.ToListAsync());
+            var userRole = GetUserRole();
+
+            if (!RolePermissionHelper.CanAccessModule(userRole, RolePermissionHelper.SystemModule.Materials))
+                return RedirectToAction("AccessDenied", "Home");
+
+            var materials = _context.TblMaterials
+                .Include(m => m.Collection)
+                .Include(m => m.Finiture)
+                .Include(m => m.Format)
+                .Include(m => m.Site)
+                .Include(m => m.User);
+
+            return View(await materials.ToListAsync());
         }
 
-        // GET: Materials/Details/5
-        public async Task<IActionResult> Details(int? id)
+        public IActionResult CreateModal()
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
+            var userRole = GetUserRole();
 
-            var tblMaterial = await _context.TblMaterials
-                .Include(t => t.Collection)
-                .Include(t => t.Finiture)
-                .Include(t => t.Format)
-                .Include(t => t.Site)
-                .Include(t => t.User)
-                .FirstOrDefaultAsync(m => m.MaterialId == id);
-            if (tblMaterial == null)
-            {
+            if (!RolePermissionHelper.CanCreate(userRole, RolePermissionHelper.SystemModule.Materials))
                 return NotFound();
-            }
 
-            return View(tblMaterial);
+            ViewData["CollectionId"] = new SelectList(
+                _context.TblCollections.Where(c => c.CollectionActive == true),
+                "CollectionId",
+                "CollectionName");
+
+            ViewData["FinitureId"] = new SelectList(
+                _context.TblFinitures.Where(f => f.FinitureActive == true),
+                "FinitureId",
+                "FinitureName");
+
+            ViewData["FormatId"] = new SelectList(
+                _context.TblFormats.Where(fo => fo.FormatActive == true),
+                "FormatId",
+                "FormatName");
+
+            ViewData["SiteId"] = new SelectList(
+                _context.TblSites.Where(s => s.SiteActive == true),
+                "SiteId",
+                "SiteName");
+
+            ViewData["UserId"] = new SelectList(
+                _context.TblUsers.Where(u => u.UserActive == true),
+                "UserId",
+                "UserName");
+
+            var model = new MaterialViewModel
+            {
+                MaterialReceivedDate = DateOnly.FromDateTime(DateTime.Now)
+            };
+            return PartialView("Create", model);
         }
 
-        // GET: Materials/Create
-        public IActionResult Create()
-        {
-            ViewData["CollectionId"] = new SelectList(_context.TblCollections, "CollectionId", "CollectionId");
-            ViewData["FinitureId"] = new SelectList(_context.TblFinitures, "FinitureId", "FinitureId");
-            ViewData["FormatId"] = new SelectList(_context.TblFormats, "FormatId", "FormatId");
-            ViewData["SiteId"] = new SelectList(_context.TblSites, "SiteId", "SiteId");
-            ViewData["UserId"] = new SelectList(_context.TblUsers, "UserId", "UserId");
-            return View();
-        }
-
-        // POST: Materials/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("MaterialId,MaterialCode,MaterialDescription,CollectionId,FinitureId,FormatId,SiteId,MaterialImg,MaterialReceivedDate,MaterialStock,UserId,RecordInsertDateTime")] TblMaterial tblMaterial)
+        public async Task<IActionResult> CreateModal(MaterialViewModel model)
         {
-            if (ModelState.IsValid)
-            {
-                _context.Add(tblMaterial);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["CollectionId"] = new SelectList(_context.TblCollections, "CollectionId", "CollectionId", tblMaterial.CollectionId);
-            ViewData["FinitureId"] = new SelectList(_context.TblFinitures, "FinitureId", "FinitureId", tblMaterial.FinitureId);
-            ViewData["FormatId"] = new SelectList(_context.TblFormats, "FormatId", "FormatId", tblMaterial.FormatId);
-            ViewData["SiteId"] = new SelectList(_context.TblSites, "SiteId", "SiteId", tblMaterial.SiteId);
-            ViewData["UserId"] = new SelectList(_context.TblUsers, "UserId", "UserId", tblMaterial.UserId);
-            return View(tblMaterial);
-        }
+            var userRole = GetUserRole();
 
-        // GET: Materials/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var tblMaterial = await _context.TblMaterials.FindAsync(id);
-            if (tblMaterial == null)
-            {
-                return NotFound();
-            }
-            ViewData["CollectionId"] = new SelectList(_context.TblCollections, "CollectionId", "CollectionId", tblMaterial.CollectionId);
-            ViewData["FinitureId"] = new SelectList(_context.TblFinitures, "FinitureId", "FinitureId", tblMaterial.FinitureId);
-            ViewData["FormatId"] = new SelectList(_context.TblFormats, "FormatId", "FormatId", tblMaterial.FormatId);
-            ViewData["SiteId"] = new SelectList(_context.TblSites, "SiteId", "SiteId", tblMaterial.SiteId);
-            ViewData["UserId"] = new SelectList(_context.TblUsers, "UserId", "UserId", tblMaterial.UserId);
-            return View(tblMaterial);
-        }
-
-        // POST: Materials/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("MaterialId,MaterialCode,MaterialDescription,CollectionId,FinitureId,FormatId,SiteId,MaterialImg,MaterialReceivedDate,MaterialStock,UserId,RecordInsertDateTime")] TblMaterial tblMaterial)
-        {
-            if (id != tblMaterial.MaterialId)
-            {
-                return NotFound();
-            }
+            if (!RolePermissionHelper.CanCreate(userRole, RolePermissionHelper.SystemModule.Materials))
+                return Json(new { success = false, message = "No tienes permiso para crear materiales." });
 
             if (ModelState.IsValid)
             {
                 try
                 {
-                    _context.Update(tblMaterial);
+                    if (model.MaterialFile != null && model.MaterialFile.Length > 0)
+                    {
+                        var uploads = Path.Combine(_environment.WebRootPath, "UploadedImages");
+                        if (!Directory.Exists(uploads)) Directory.CreateDirectory(uploads);
+
+                        var fileName = Path.GetFileName(model.MaterialFile.FileName);
+                        var filePath = Path.Combine(uploads, fileName);
+
+                        using (var stream = new FileStream(filePath, FileMode.Create))
+                        {
+                            await model.MaterialFile.CopyToAsync(stream);
+                        }
+
+                        model.MaterialImg = fileName;
+                    }
+
+                    var userName = User?.Identity?.Name;
+                    var currentUser = await _context.TblUsers
+                        .FirstOrDefaultAsync(u => u.UserName == userName);
+
+                    var entity = new TblMaterial
+                    {
+                        MaterialCode = model.MaterialCode,
+                        MaterialDescription = model.MaterialDescription,
+                        CollectionId = model.CollectionId,
+                        FinitureId = model.FinitureId,
+                        FormatId = model.FormatId,
+                        SiteId = model.SiteId,
+                        MaterialImg = model.MaterialImg,
+                        MaterialReceivedDate = model.MaterialReceivedDate,
+                        MaterialStock = model.MaterialStock,
+                        UserId = currentUser?.UserId ?? 0,
+                    };
+
+                    _context.Add(entity);
                     await _context.SaveChangesAsync();
+                    return Json(new { success = true, message = "Material creado correctamente." });
                 }
-                catch (DbUpdateConcurrencyException)
+                catch (Exception ex)
                 {
-                    if (!TblMaterialExists(tblMaterial.MaterialId))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
+                    return Json(new { success = false, message = ex.Message });
                 }
-                return RedirectToAction(nameof(Index));
             }
-            ViewData["CollectionId"] = new SelectList(_context.TblCollections, "CollectionId", "CollectionId", tblMaterial.CollectionId);
-            ViewData["FinitureId"] = new SelectList(_context.TblFinitures, "FinitureId", "FinitureId", tblMaterial.FinitureId);
-            ViewData["FormatId"] = new SelectList(_context.TblFormats, "FormatId", "FormatId", tblMaterial.FormatId);
-            ViewData["SiteId"] = new SelectList(_context.TblSites, "SiteId", "SiteId", tblMaterial.SiteId);
-            ViewData["UserId"] = new SelectList(_context.TblUsers, "UserId", "UserId", tblMaterial.UserId);
-            return View(tblMaterial);
+
+            ViewData["CollectionId"] = new SelectList(_context.TblCollections, "CollectionId", "CollectionName", model.CollectionId);
+            ViewData["FinitureId"] = new SelectList(_context.TblFinitures, "FinitureId", "FinitureName", model.FinitureId);
+            ViewData["FormatId"] = new SelectList(_context.TblFormats, "FormatId", "FormatName", model.FormatId);
+            ViewData["SiteId"] = new SelectList(_context.TblSites, "SiteId", "SiteName", model.SiteId);
+
+            return PartialView("Create", model);
         }
 
-        // GET: Materials/Delete/5
-        public async Task<IActionResult> Delete(int? id)
+        public async Task<IActionResult> EditModal(int? id)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
+            var userRole = GetUserRole();
 
-            var tblMaterial = await _context.TblMaterials
-                .Include(t => t.Collection)
-                .Include(t => t.Finiture)
-                .Include(t => t.Format)
-                .Include(t => t.Site)
-                .Include(t => t.User)
-                .FirstOrDefaultAsync(m => m.MaterialId == id);
-            if (tblMaterial == null)
-            {
+            if (!RolePermissionHelper.CanEdit(userRole, RolePermissionHelper.SystemModule.Materials))
                 return NotFound();
-            }
 
-            return View(tblMaterial);
+            if (id == null) return NotFound();
+
+            var entity = await _context.TblMaterials.FindAsync(id);
+            if (entity == null) return NotFound();
+
+            var model = new MaterialViewModel
+            {
+                MaterialId = entity.MaterialId,
+                MaterialCode = entity.MaterialCode,
+                MaterialDescription = entity.MaterialDescription,
+                CollectionId = entity.CollectionId,
+                FinitureId = entity.FinitureId,
+                FormatId = entity.FormatId,
+                SiteId = entity.SiteId,
+                MaterialImg = entity.MaterialImg,
+                MaterialReceivedDate = entity.MaterialReceivedDate,
+                MaterialStock = entity.MaterialStock,
+                UserId = entity.UserId,
+            };
+
+            ViewData["CollectionId"] = new SelectList(
+                _context.TblCollections.Where(c => c.CollectionActive == true),
+                "CollectionId",
+                "CollectionName",
+                model.CollectionId);
+
+            ViewData["FinitureId"] = new SelectList(
+                _context.TblFinitures.Where(f => f.FinitureActive == true),
+                "FinitureId",
+                "FinitureName",
+                model.FinitureId);
+
+            ViewData["FormatId"] = new SelectList(
+                _context.TblFormats.Where(fo => fo.FormatActive == true),
+                "FormatId",
+                "FormatName",
+                model.FormatId);
+
+            ViewData["SiteId"] = new SelectList(
+                _context.TblSites.Where(s => s.SiteActive == true),
+                "SiteId",
+                "SiteName",
+                model.SiteId);
+
+            ViewData["UserId"] = new SelectList(
+                _context.TblUsers.Where(u => u.UserActive == true),
+                "UserId",
+                "UserName",
+                model.UserId);
+
+            return PartialView("Edit", model);
         }
 
-        // POST: Materials/Delete/5
-        [HttpPost, ActionName("Delete")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditModal(int id, MaterialViewModel model)
+        {
+            var userRole = GetUserRole();
+
+            if (!RolePermissionHelper.CanEdit(userRole, RolePermissionHelper.SystemModule.Materials))
+                return Json(new { success = false, message = "No tienes permiso para editar materiales." });
+
+            if (id != model.MaterialId) return NotFound();
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var entity = await _context.TblMaterials.FindAsync(id);
+                    if (entity == null) return NotFound();
+
+                    if (model.MaterialFile != null && model.MaterialFile.Length > 0)
+                    {
+                        var uploads = Path.Combine(_environment.WebRootPath, "UploadedImages");
+                        if (!Directory.Exists(uploads)) Directory.CreateDirectory(uploads);
+
+                        var fileName = Path.GetFileName(model.MaterialFile.FileName);
+                        var filePath = Path.Combine(uploads, fileName);
+
+                        using (var stream = new FileStream(filePath, FileMode.Create))
+                        {
+                            await model.MaterialFile.CopyToAsync(stream);
+                        }
+
+                        entity.MaterialImg = fileName;
+                    }
+
+                    entity.MaterialCode = model.MaterialCode;
+                    entity.MaterialDescription = model.MaterialDescription;
+                    entity.CollectionId = model.CollectionId;
+                    entity.FinitureId = model.FinitureId;
+                    entity.FormatId = model.FormatId;
+                    entity.SiteId = model.SiteId;
+                    entity.MaterialReceivedDate = model.MaterialReceivedDate;
+                    entity.MaterialStock = model.MaterialStock;
+
+                    _context.Update(entity);
+                    await _context.SaveChangesAsync();
+
+                    return Json(new { success = true, message = "Material actualizado correctamente." });
+                }
+                catch (Exception ex)
+                {
+                    return Json(new { success = false, message = ex.Message });
+                }
+            }
+
+            ViewData["CollectionId"] = new SelectList(_context.TblCollections, "CollectionId", "CollectionName", model.CollectionId);
+            ViewData["FinitureId"] = new SelectList(_context.TblFinitures, "FinitureId", "FinitureName", model.FinitureId);
+            ViewData["FormatId"] = new SelectList(_context.TblFormats, "FormatId", "FormatName", model.FormatId);
+            ViewData["SiteId"] = new SelectList(_context.TblSites, "SiteId", "SiteName", model.SiteId);
+
+            return PartialView("Edit", model);
+        }
+
+        public async Task<IActionResult> DeleteModal(int? id)
+        {
+            var userRole = GetUserRole();
+
+            if (!RolePermissionHelper.CanDelete(userRole, RolePermissionHelper.SystemModule.Materials))
+                return NotFound();
+
+            if (id == null) return NotFound();
+
+            var entity = await _context.TblMaterials
+                .Include(m => m.Collection)
+                .Include(m => m.Finiture)
+                .Include(m => m.Format)
+                .Include(m => m.Site)
+                .FirstOrDefaultAsync(m => m.MaterialId == id);
+
+            if (entity == null) return NotFound();
+
+            return PartialView("Delete", entity);
+        }
+
+        [HttpPost, ActionName("DeleteModal")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            var tblMaterial = await _context.TblMaterials.FindAsync(id);
-            if (tblMaterial != null)
-            {
-                _context.TblMaterials.Remove(tblMaterial);
-            }
+            var userRole = GetUserRole();
 
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
+            if (!RolePermissionHelper.CanDelete(userRole, RolePermissionHelper.SystemModule.Materials))
+                return Json(new { success = false, message = "No tienes permiso para eliminar materiales." });
+
+            try
+            {
+                var entity = await _context.TblMaterials.FindAsync(id);
+                if (entity != null)
+                {
+                    _context.TblMaterials.Remove(entity);
+                    await _context.SaveChangesAsync();
+                }
+
+                return Json(new { success = true, message = "Material eliminado correctamente." });
+            }
+            catch (Exception ex)
+            {
+                return Json(new { success = false, message = ex.Message });
+            }
         }
 
         private bool TblMaterialExists(int id)
--- ViewModels/MaterialViewModel.cs
+++ ViewModels/MaterialViewModel.cs
@@ -1,6 +1,54 @@
-﻿namespace InventorySys.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InventorySys.ViewModels
 {
     public class MaterialViewModel
     {
+        public int MaterialId { get; set; }
+
+        [Required]
+        [Display(Name = "Código de Material")]
+        public string MaterialCode { get; set; } = null!;
+
+        [Required]
+        [Display(Name = "Descripción")]
+        public string MaterialDescription { get; set; } = null!;
+
+        [Display(Name = "Colección")]
+        public int? CollectionId { get; set; }
+
+        [Display(Name = "Acabado")]
+        public int? FinitureId { get; set; }
+
+        [Display(Name = "Formato")]
+        public int? FormatId { get; set; }
+
+        [Display(Name = "Sitio")]
+        public int? SiteId { get; set; }
+
+        [Display(Name = "Imagen")]
+        public string? MaterialImg { get; set; }
+
+        [Display(Name = "Subir Imagen")]
+        public IFormFile? MaterialFile { get; set; }
+
+        [Display(Name = "Fecha de Arribo")]
+        [DataType(DataType.Date)]
+        public DateOnly MaterialReceivedDate { get; set; }
+
+        [Display(Name = "Stock")]
+        [DataType(DataType.Currency)]
+        public decimal MaterialStock { get; set; }
+
+        [Display(Name = "Usuario")]
+        public int? UserId { get; set; }
+
+        public DateTime RecordInsertDateTime { get; set; }
+        // Propiedades para mostrar nombres
+        // Propiedades nullables para mostrar nombres
+        public string? CollectionName { get; set; }
+        public string? FinitureName { get; set; }
+        public string? FormatName { get; set; }
+        public string? SiteName { get; set; }
     }
 }
