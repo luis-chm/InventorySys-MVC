4d21df86853b7318baac87c04edbc2933b5c017b
akanneee123 <akane26allen@gmail.com>
2025-11-03 19:26:39 -0600

    Feature: Se genera controlador y vista por defecto

---
+++ Controllers/ReportsController.cs
@@ -0,0 +1,25 @@
+﻿using InventorySys.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InventorySys.Controllers
+{
+    public class ReportsController : Controller
+    {
+        private readonly InventorySysContext _context;
+
+        public ReportsController(InventorySysContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult ReportsMaterials()
+        {
+            return View();
+        }
+
+        public IActionResult ReportsTransactions()
+        {
+            return View();
+        }
+    }
+}
